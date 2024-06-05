fd63e44d6fe3e3867a752c75817af61e2f4e80bc
0xvoidmain <hoangtung.utc@gmail.com>
2024-06-05 21:26:50 +0700

    update component valuedisplay

--- src/components/Pool/PoolCard.tsx
+++ src/components/Pool/PoolCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Card, Stack } from "@mui/joy";
 import React from "react";
-import ValueSmallDisplay from "../ValueSmallDisplay";
+import ValueDisplay from "../ValueDisplay";
 import { IconDailyReward, IconMarPoint, IconPending, IconTotalValueStake, IconUSDT, IconYourDailyReward, IconYourDeposited } from "../../icons";
 
 const PoolCard = (props:any) => {
@@ -11,13 +11,13 @@ const PoolCard = (props:any) => {
         borderRadius: '12px'
     }}>
         <Box>
-            <ValueSmallDisplay name="Total value Staked" text="$20,000,000" icon={IconTotalValueStake} />
-            <ValueSmallDisplay name="Base Points Per Dollar" text="0.01 PTS" icon={IconDailyReward} marginTop={1.5}/>
+            <ValueDisplay variant="small" name="Total value Staked" text="$20,000,000" icon={IconTotalValueStake} />
+            <ValueDisplay variant="small" name="Base Points Per Dollar" text="0.01 PTS" icon={IconDailyReward} marginTop={1.5}/>
 
-            <ValueSmallDisplay name="Your Value Staked" text="$200.5" icon={IconYourDeposited} marginTop={1.5}/>
-            <ValueSmallDisplay name="Your Daily Reward" text="14 PTS" icon={IconYourDailyReward} marginTop={1.5}/>
+            <ValueDisplay variant="small" name="Your Value Staked" text="$200.5" icon={IconYourDeposited} marginTop={1.5}/>
+            <ValueDisplay variant="small" name="Your Daily Reward" text="14 PTS" icon={IconYourDailyReward} marginTop={1.5}/>
 
-            <ValueSmallDisplay name="Your NFTs Deposited" icon={IconYourDeposited} marginTop={1.5} images={[
+            <ValueDisplay variant="small" name="Your NFTs Deposited" icon={IconYourDeposited} marginTop={1.5} images={[
                 'https://i.seadn.io/gae/WG55wHkFEYqegub2kkMZbUJwmI0TfW75LRrgI4odvsfqZ0sTEX9CCr3QUenYrf9tzIsSUp7vNccImZtDO-kcTLzsqxlb98DKiO2mLOk',
                 'https://i.seadn.io/gae/WG55wHkFEYqegub2kkMZbUJwmI0TfW75LRrgI4odvsfqZ0sTEX9CCr3QUenYrf9tzIsSUp7vNccImZtDO-kcTLzsqxlb98DKiO2mLOk',
                 'https://i.seadn.io/gae/WG55wHkFEYqegub2kkMZbUJwmI0TfW75LRrgI4odvsfqZ0sTEX9CCr3QUenYrf9tzIsSUp7vNccImZtDO-kcTLzsqxlb98DKiO2mLOk',
--- src/components/ValueDisplay/index.tsx
+++ src/components/ValueDisplay/index.tsx
@@ -2,11 +2,13 @@ import { Box, Stack, StackProps, Typography } from "@mui/joy";
 import React, { memo } from "react";
 
 export default memo<{
-    text: string
+    text?: string
     name?: string
     icon?: string
     images?: string[]
-} & StackProps>(({icon, text, name, images, ...stackProps}) => {
+    variant?: 'medium' | 'small'
+} & StackProps>(({icon, text, name, images, variant = 'medium', ...stackProps}) => {
+    const sizeImage = variant == 'medium' ? '40px' : '25px'
     return (
         <Stack sx={{
             background: '#F5F5F5',
@@ -18,17 +20,27 @@ export default memo<{
                     {icon && <img src={icon} width={40} />}
                 </Box>
                 <Box>
-                    <Typography level="title-lg">{text}</Typography>
-                    {name && <Typography level="body-sm" color="neutral">{name}</Typography>}
+                    {variant == 'medium' && <Typography level="title-lg">{text}</Typography>}
+                    {variant == 'medium' && name && <Typography level="body-sm" color="neutral">{name}</Typography>}
+                    {variant == 'small' && name && <Typography level="title-lg">{name}</Typography>}
                 </Box>
             </Stack>
-            {images && images.length > 0 &&
-                <Stack direction={"row"} alignItems={"center"} justifyContent={"flex-end"} flexWrap={"wrap"}>
-                    {images.map(e => (
-                        <img src={e} width={40} height={40} style={{objectFit: 'cover', margin: 2, borderRadius: 5}}/>
-                    ))}
-                </Stack>
-            }
+            <Stack direction={"column"} alignItems={'flex-end'}>
+                {variant=='small' && text && <Typography level="title-lg" textAlign={'left'}>{text}</Typography>}
+                {images && images.length > 0 &&
+                    <Stack direction={"row"} alignItems={"center"} justifyContent={"flex-end"} flexWrap={"wrap"}>
+                        {images.slice(0, 3).map(e => (
+                            <img src={e} width={sizeImage} height={sizeImage} style={{objectFit: 'cover', margin: 2, borderRadius: 5}}/>
+                        ))}
+                        {images.length > 3 && (
+                            <Stack alignItems={"center"} justifyContent={"center"} sx={{background: 'white', borderRadius: 5, margin: '2px'}} height={sizeImage} width={sizeImage}>
+                                <Typography fontSize={"10px"} fontWeight={700}>+{images.length - 3}</Typography>
+                            </Stack>
+                        )}
+                    </Stack>
+                }
+            </Stack>
+
         </Stack>
     )
 })
