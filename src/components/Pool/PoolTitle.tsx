f56d3cedae9f76bdf21e2f66069c7b5b5e6a84af
0xvoidmain <hoangtung.utc@gmail.com>
2024-07-17 23:39:22 +0700

    update

--- src/components/Pool/PoolTitle.tsx
+++ src/components/Pool/PoolTitle.tsx
@@ -9,7 +9,7 @@ const PoolTitle = ({assetSymbol} : {assetSymbol: string}) => {
         <Typography fontWeight={700} fontSize={20}>
           {assetSymbol}
         </Typography>
-        {/* <img src={TokenToIcon[assetSymbol]} width={20} height={20} /> */}
+        <img src={TokenToIcon['ETH']} width={20} height={20} />
       </Stack>
       {/* <Stack direction="row" gap={1} alignItems="center">
         <BootsTag />
