0717f1f15fefb333fdc8a1ce8506f453b6bdac80
0xvoidmain <hoangtung.utc@gmail.com>
2024-08-15 22:08:38 +0700

    update sepolia rpc

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,11 +3,11 @@ import { sepolia, mainnet } from 'wagmi/chains'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 
 // console.log(sepolia)
-// sepolia.rpcUrls = {
-//   default: {
-//     http: ['https://sepolia.infura.io/v3/' as any],
-//   },
-// };
+sepolia.rpcUrls = {
+  default: {
+    http: ['https://sepolia.infura.io/v3/c867f71cca3449dfbbfa806e2fea931e' as any],
+  },
+};
 
 export const wagmiConfig = createConfig({
   chains: [sepolia],
